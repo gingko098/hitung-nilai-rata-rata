3c257dd2a11b1439076f7c2d28b4ec73d512da83
gingko098 <fjummadil@gmail.com>
2022-01-28 15:11:11 +0700

    hitung nilai

---
+++ Form1.Designer.vb
@@ -0,0 +1,398 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnkeluar = New System.Windows.Forms.Button()
+        Me.cmdreset = New System.Windows.Forms.Button()
+        Me.txtHURUF = New System.Windows.Forms.TextBox()
+        Me.txtRATA = New System.Windows.Forms.TextBox()
+        Me.txtrataP = New System.Windows.Forms.TextBox()
+        Me.txtrataaktif = New System.Windows.Forms.TextBox()
+        Me.txtrataPP = New System.Windows.Forms.TextBox()
+        Me.txtrataTP = New System.Windows.Forms.TextBox()
+        Me.txtrataUAS = New System.Windows.Forms.TextBox()
+        Me.txtrataUTS = New System.Windows.Forms.TextBox()
+        Me.txtpresent = New System.Windows.Forms.TextBox()
+        Me.txtaktif = New System.Windows.Forms.TextBox()
+        Me.txtPP = New System.Windows.Forms.TextBox()
+        Me.txtTP = New System.Windows.Forms.TextBox()
+        Me.txtUAS = New System.Windows.Forms.TextBox()
+        Me.txtUTS = New System.Windows.Forms.TextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtP = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmdHASIL = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Panel1.Controls.Add(Me.cmdHASIL)
+        Me.Panel1.Controls.Add(Me.btnkeluar)
+        Me.Panel1.Controls.Add(Me.cmdreset)
+        Me.Panel1.Controls.Add(Me.txtHURUF)
+        Me.Panel1.Controls.Add(Me.txtRATA)
+        Me.Panel1.Controls.Add(Me.txtrataP)
+        Me.Panel1.Controls.Add(Me.txtrataaktif)
+        Me.Panel1.Controls.Add(Me.txtrataPP)
+        Me.Panel1.Controls.Add(Me.txtrataTP)
+        Me.Panel1.Controls.Add(Me.txtrataUAS)
+        Me.Panel1.Controls.Add(Me.txtrataUTS)
+        Me.Panel1.Controls.Add(Me.txtpresent)
+        Me.Panel1.Controls.Add(Me.txtaktif)
+        Me.Panel1.Controls.Add(Me.txtPP)
+        Me.Panel1.Controls.Add(Me.txtTP)
+        Me.Panel1.Controls.Add(Me.txtUAS)
+        Me.Panel1.Controls.Add(Me.txtUTS)
+        Me.Panel1.Controls.Add(Me.Label14)
+        Me.Panel1.Controls.Add(Me.Label13)
+        Me.Panel1.Controls.Add(Me.Label12)
+        Me.Panel1.Controls.Add(Me.Label11)
+        Me.Panel1.Controls.Add(Me.Label10)
+        Me.Panel1.Controls.Add(Me.Label9)
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.txtP)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(1, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(549, 406)
+        Me.Panel1.TabIndex = 0
+        '
+        'btnkeluar
+        '
+        Me.btnkeluar.Location = New System.Drawing.Point(392, 356)
+        Me.btnkeluar.Name = "btnkeluar"
+        Me.btnkeluar.Size = New System.Drawing.Size(89, 26)
+        Me.btnkeluar.TabIndex = 30
+        Me.btnkeluar.Text = "KELUAR"
+        Me.btnkeluar.UseVisualStyleBackColor = True
+        '
+        'cmdreset
+        '
+        Me.cmdreset.Location = New System.Drawing.Point(300, 356)
+        Me.cmdreset.Name = "cmdreset"
+        Me.cmdreset.Size = New System.Drawing.Size(86, 26)
+        Me.cmdreset.TabIndex = 29
+        Me.cmdreset.Text = "RESET"
+        Me.cmdreset.UseVisualStyleBackColor = True
+        '
+        'txtHURUF
+        '
+        Me.txtHURUF.Location = New System.Drawing.Point(321, 303)
+        Me.txtHURUF.Name = "txtHURUF"
+        Me.txtHURUF.Size = New System.Drawing.Size(124, 20)
+        Me.txtHURUF.TabIndex = 27
+        '
+        'txtRATA
+        '
+        Me.txtRATA.Location = New System.Drawing.Point(192, 303)
+        Me.txtRATA.Name = "txtRATA"
+        Me.txtRATA.Size = New System.Drawing.Size(124, 20)
+        Me.txtRATA.TabIndex = 26
+        '
+        'txtrataP
+        '
+        Me.txtrataP.Location = New System.Drawing.Point(357, 206)
+        Me.txtrataP.Name = "txtrataP"
+        Me.txtrataP.Size = New System.Drawing.Size(124, 20)
+        Me.txtrataP.TabIndex = 25
+        '
+        'txtrataaktif
+        '
+        Me.txtrataaktif.Location = New System.Drawing.Point(357, 180)
+        Me.txtrataaktif.Name = "txtrataaktif"
+        Me.txtrataaktif.Size = New System.Drawing.Size(124, 20)
+        Me.txtrataaktif.TabIndex = 24
+        '
+        'txtrataPP
+        '
+        Me.txtrataPP.Location = New System.Drawing.Point(357, 156)
+        Me.txtrataPP.Name = "txtrataPP"
+        Me.txtrataPP.Size = New System.Drawing.Size(124, 20)
+        Me.txtrataPP.TabIndex = 23
+        '
+        'txtrataTP
+        '
+        Me.txtrataTP.Location = New System.Drawing.Point(357, 129)
+        Me.txtrataTP.Name = "txtrataTP"
+        Me.txtrataTP.Size = New System.Drawing.Size(124, 20)
+        Me.txtrataTP.TabIndex = 22
+        '
+        'txtrataUAS
+        '
+        Me.txtrataUAS.Location = New System.Drawing.Point(357, 102)
+        Me.txtrataUAS.Name = "txtrataUAS"
+        Me.txtrataUAS.Size = New System.Drawing.Size(124, 20)
+        Me.txtrataUAS.TabIndex = 21
+        '
+        'txtrataUTS
+        '
+        Me.txtrataUTS.Location = New System.Drawing.Point(357, 71)
+        Me.txtrataUTS.Name = "txtrataUTS"
+        Me.txtrataUTS.Size = New System.Drawing.Size(124, 20)
+        Me.txtrataUTS.TabIndex = 20
+        '
+        'txtpresent
+        '
+        Me.txtpresent.Location = New System.Drawing.Point(176, 204)
+        Me.txtpresent.Name = "txtpresent"
+        Me.txtpresent.Size = New System.Drawing.Size(124, 20)
+        Me.txtpresent.TabIndex = 19
+        '
+        'txtaktif
+        '
+        Me.txtaktif.Location = New System.Drawing.Point(176, 180)
+        Me.txtaktif.Name = "txtaktif"
+        Me.txtaktif.Size = New System.Drawing.Size(124, 20)
+        Me.txtaktif.TabIndex = 18
+        '
+        'txtPP
+        '
+        Me.txtPP.Location = New System.Drawing.Point(176, 152)
+        Me.txtPP.Name = "txtPP"
+        Me.txtPP.Size = New System.Drawing.Size(124, 20)
+        Me.txtPP.TabIndex = 17
+        '
+        'txtTP
+        '
+        Me.txtTP.Location = New System.Drawing.Point(176, 125)
+        Me.txtTP.Name = "txtTP"
+        Me.txtTP.Size = New System.Drawing.Size(124, 20)
+        Me.txtTP.TabIndex = 16
+        '
+        'txtUAS
+        '
+        Me.txtUAS.Location = New System.Drawing.Point(176, 97)
+        Me.txtUAS.Name = "txtUAS"
+        Me.txtUAS.Size = New System.Drawing.Size(124, 20)
+        Me.txtUAS.TabIndex = 15
+        '
+        'txtUTS
+        '
+        Me.txtUTS.Location = New System.Drawing.Point(176, 71)
+        Me.txtUTS.Name = "txtUTS"
+        Me.txtUTS.Size = New System.Drawing.Size(124, 20)
+        Me.txtUTS.TabIndex = 14
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(84, 306)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(102, 13)
+        Me.Label14.TabIndex = 13
+        Me.Label14.Text = "Rata-rata Nilai Anda"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(318, 213)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(33, 13)
+        Me.Label13.TabIndex = 12
+        Me.Label13.Text = "(10%)"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(318, 187)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(33, 13)
+        Me.Label12.TabIndex = 11
+        Me.Label12.Text = "(10%)"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(318, 159)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(33, 13)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "(15%)"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(318, 109)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(33, 13)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "(20%)"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(318, 136)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(33, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "(25%)"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(318, 78)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(33, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "(20%)"
+        '
+        'txtP
+        '
+        Me.txtP.AutoSize = True
+        Me.txtP.Location = New System.Drawing.Point(114, 213)
+        Me.txtP.Name = "txtP"
+        Me.txtP.Size = New System.Drawing.Size(56, 13)
+        Me.txtP.TabIndex = 6
+        Me.txtP.Text = "Presentasi"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(118, 187)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(52, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Keaktifan"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(84, 159)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(86, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Presentasi Prject"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(97, 132)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(73, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Tugas Project"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(65, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(105, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Ujian Akhir Semester"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(46, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Ujian Tengah Semeseter"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(143, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(290, 19)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "PERHITUNGAN NILAI MATA KULIAH"
+        '
+        'cmdHASIL
+        '
+        Me.cmdHASIL.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdHASIL.Location = New System.Drawing.Point(308, 257)
+        Me.cmdHASIL.Name = "cmdHASIL"
+        Me.cmdHASIL.Size = New System.Drawing.Size(75, 23)
+        Me.cmdHASIL.TabIndex = 31
+        Me.cmdHASIL.Text = "Hasil"
+        Me.cmdHASIL.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(548, 410)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "PERHITUNGAN NILAI MATA KULIAH"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btnkeluar As System.Windows.Forms.Button
+    Friend WithEvents cmdreset As System.Windows.Forms.Button
+    Friend WithEvents txtHURUF As System.Windows.Forms.TextBox
+    Friend WithEvents txtRATA As System.Windows.Forms.TextBox
+    Friend WithEvents txtrataP As System.Windows.Forms.TextBox
+    Friend WithEvents txtrataaktif As System.Windows.Forms.TextBox
+    Friend WithEvents txtrataPP As System.Windows.Forms.TextBox
+    Friend WithEvents txtrataTP As System.Windows.Forms.TextBox
+    Friend WithEvents txtrataUAS As System.Windows.Forms.TextBox
+    Friend WithEvents txtrataUTS As System.Windows.Forms.TextBox
+    Friend WithEvents txtpresent As System.Windows.Forms.TextBox
+    Friend WithEvents txtaktif As System.Windows.Forms.TextBox
+    Friend WithEvents txtPP As System.Windows.Forms.TextBox
+    Friend WithEvents txtTP As System.Windows.Forms.TextBox
+    Friend WithEvents txtUAS As System.Windows.Forms.TextBox
+    Friend WithEvents txtUTS As System.Windows.Forms.TextBox
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents txtP As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmdHASIL As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,63 @@
+﻿
+Public Class Form1
+
+    Private Sub txtUTS_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtUTS.TextChanged
+        Const UTS As Single = 0.2
+        Dim rata_uts As Single
+        rata_uts = txtUTS.Text
+        txtrataUTS.Text = UTS * rata_uts
+    End Sub
+
+    Private Sub txtUAS_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtUAS.TextChanged
+        Const UAS As Single = 0.2
+        Dim rata_uas As Single
+        rata_uas = txtUAS.Text
+        txtrataUAS.Text = UAS * rata_uas
+    End Sub
+
+    Private Sub txtTP_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtTP.TextChanged
+        Const Project As Single = (25 / 100)
+        Dim rata_project As Single
+        rata_project = txtTP.Text
+        txtrataTP.Text = Project * rata_project
+    End Sub
+
+    Private Sub txtPP_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtPP.TextChanged
+        Const Presentasi_Project As Single = (15 / 100)
+        Dim rata_pp As Single
+        rata_pp = txtPP.Text
+        txtrataPP.Text = Presentasi_Project * rata_pp
+    End Sub
+
+    Private Sub txtaktif_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtaktif.TextChanged
+        Const Keaktifan As Single = (10 / 100)
+        Dim rata_keaktifan As Single
+        rata_keaktifan = txtaktif.Text
+        txtrataaktif.Text = Keaktifan * rata_keaktifan
+    End Sub
+
+    Private Sub TextBox6_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtpresent.TextChanged
+        Const Presentasi As Single = (10 / 100)
+        Dim rata_presentasi As Single
+        rata_presentasi = txtpresent.Text
+        txtrataP.Text = Presentasi * rata_presentasi
+    End Sub
+
+    Private Sub cmdHASIL_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdHASIL.Click
+        txtRATA.Text = Val(txtrataUTS.Text) + Val(txtrataUAS.Text) + Val(txtrataTP.Text) + Val(txtrataPP.Text) + Val(txtrataaktif.Text) + Val(txtrataP.Text)
+        If txtRATA.Text >= 85 Then
+            txtHURUF.Text = "A"
+        ElseIf txtRATA.Text >= 75 Then
+            txtHURUF.Text = "B"
+        ElseIf txtRATA.Text >= 60 Then
+            txtHURUF.Text = "C"
+        ElseIf txtRATA.Text >= 50 Then
+            txtHURUF.Text = "D"
+        Else
+            txtHURUF.Text = "E"
+        End If
+
+    End Sub
+End Class
+
+
